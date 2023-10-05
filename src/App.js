96074620cba9cec93ef7cd78709d8abb460d079d
Awais Khalid <142379555+Awais-Khalid123@users.noreply.github.com>
2023-10-05 18:32:57 +0500

    my cool calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [result,setResult] = useState("")
+  console.log(result)
+
+  const handleOnclick =(event)=>{
+    setResult(result.concat(event.target.value))
+  }
+
+  const handleBackspace = () => {
+    if (result.length > 0) {
+     
+      setResult(result.slice(0, -1));
+    }
+  };
+
+  const handleToggleSign = () => {
+    if (result !== '') {
+      if (result[0] === '-') {  
+        setResult(result.slice(1));
+      } else {   
+        setResult('-' + result);
+      }
+    }
+  };
+
+
+
+  const calc =()=>{
+setResult(eval(result).toString())
+  }
+  const clearDisplay=()=>{
+    setResult("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+<div className="main">
+    <input  value={result} className="display" type="text" placeholder='0'/>
+    <input className='button grey' type="button" onClick={clearDisplay} value={"AC"}/>
+    <button className='button grey fnt' onClick={handleBackspace}><i className="fnt fas fa-backspace"></i></button>
+    <input className='button grey'type='button' onClick={handleToggleSign} value={"+/-"}/>
+    <input className='button yellow' type='button' onClick={handleOnclick} value={"/"}/>
+
+    <input className='button'type='button' onClick={handleOnclick} value={"7"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"8"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"9"}/>
+    <input className='button yellow'type='button' onClick={handleOnclick} value={"*"}/>
+
+    <input className='button'type='button' onClick={handleOnclick} value={"4"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"5"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"6"}/>
+    <input className='button yellow'type='button' onClick={handleOnclick} value={"+"}/>
+
+    <input className='button'type='button' onClick={handleOnclick} value={"1"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"2"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"3"}/>
+    <input className='button yellow'type='button' onClick={handleOnclick} value={"-"}/>
+
+    <input className='button equal'type='button' onClick={handleOnclick} value={"0"}/>
+    <input className='button'type='button' onClick={handleOnclick} value={"."}/>
+    <input className='button  white'type='button' onClick={calc} value={"="}/>
+    
     </div>
+    </>
   );
 }
 
